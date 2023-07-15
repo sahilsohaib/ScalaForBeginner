3e26136deb4fb9653d30c23995cc14e854186e5f
sahil sohaib ali <lihassohaib@gmail.com>
2023-07-15 23:26:46 +0530

    variable

---
+++ src/main/scala/scalaForBigenner/variable.scala
@@ -0,0 +1,15 @@
+package scalaForBigenner
+
+object variable {
+  def main(args: Array[String]): Unit = {
+    /*
+    var myVar :Int =70
+    val myVal :Int =80
+    println(myVar + myVal)
+    */
+
+    var number1 :Int =90
+    val name1 : String="Sahil"
+    println(number1+name1)
+  }
+}
