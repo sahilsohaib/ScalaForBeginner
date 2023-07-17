2c0a1fd7fed26f6e3c20ef7c226da27a89c9a092
sahil sohaib ali <lihassohaib@gmail.com>
2023-07-18 01:11:54 +0530

    add videos

---
+++ src/main/scala/scalaForBigenner/vid7_do_while_loop.scala
@@ -0,0 +1,15 @@
+package scalaForBigenner
+
+object vid7_do_while_loop {
+  def main(args: Array[String]): Unit = {
+    var x: Int = 10
+    do{
+      x=x+1
+      println("the value of the x is " +x)
+    }while(x<20)
+
+
+    
+  }
+
+}
---
+++ src/main/scala/scalaForBigenner/vid8_while_loop.scala
@@ -0,0 +1,11 @@
+package scalaForBigenner
+
+object vid8_while_loop {
+  def main(args: Array[String]): Unit = {
+    var x:Int =10
+    while(x<35){
+      println("Print the value of x " +x)
+      x=x+1
+    }
+  }
+}
