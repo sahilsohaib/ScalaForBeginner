374a8595d8f5f17f20c8a4ce7dd2713a48b1635f
sahil sohaib ali <lihassohaib@gmail.com>
2023-07-19 02:58:43 +0530

    add vid 11

--- src/main/scala/scalaForBigenner/vid11_array.scala
+++ src/main/scala/scalaForBigenner/vid11_array.scala
@@ -3,23 +3,25 @@ package scalaForBigenner
 object vid11_array {
   def main(args: Array[String]): Unit = {
 
-    var myList = Array(1, 2, 3, 4, 5, 6, 7, 8, 9)
+    var myList = Array(1, 2,2, 3, 4, 5, 6, 7, 8, 9)
 
     for (x <- myList) {
       println(x)
     }
     var total = 0
-    for (i <- 0 to (myList.length - 1)) {
-      // if we write total= total + i    then answer is 36 why???
-      total = total + myList(i)
+    for (i <- myList) {
+      total = total + i
     }
     println("total is " + total)
-
     var max = myList(0)
-    for (i <- 1 to (myList.length - 1)) {
-      if (myList(i) > max) max = myList(i)
+    for (i <- myList) {
+      if (i > max)
+        max = i
 
     }
-    println("the max is " +max)
+    println("the max is " + max)
+
+
   }
+
 }
